32bffd01ea5f0babb9bb13704c3eb55d254f1dd7
lloptyr <dustyn888@yahoo.com>
2021-05-19 14:15:16 -0700

    first commit

---
+++ loot_table_base.java
@@ -0,0 +1,12 @@
+package Loot_Table_project;
+public class loot_table_base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		frame loot = new frame();
+		loot.setVisible(true);
+		
+	}
+	
+
+}
